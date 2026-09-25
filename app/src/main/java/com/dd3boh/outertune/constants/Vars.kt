d7212f540da09e439d2d556b34618fea31622c5d
dev <dev@local.com>
2026-09-25 15:48:30 +0800

    feat: 播放器背景默认跟随封面取色（对标椒盐）

--- app/src/main/java/com/dd3boh/outertune/constants/Vars.kt
+++ app/src/main/java/com/dd3boh/outertune/constants/Vars.kt
@@ -55,8 +55,7 @@ const val MAX_PLAYER_CONSECUTIVE_ERR = 3
  * Misc weird constants
  */
 
-val DEFAULT_PLAYER_BACKGROUND =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PlayerBackgroundStyle.BLUR else PlayerBackgroundStyle.GRADIENT
+val DEFAULT_PLAYER_BACKGROUND = PlayerBackgroundStyle.FOLLOW_THEME
 
 val scannerWhitelistExts = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
     listOf("dsf", "dff", "xm", "mod", "tta", "ape", "wv")
