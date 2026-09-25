15fa8c9f9d4fc95de704e9d81f59ee3b77bd5bdb
jinghouyun <jinghouyun@github.com>
2026-09-25 22:09:18 +0800

    fix: 补回 LinearProgressIndicator 导入

--- app/src/main/java/com/dd3boh/outertune/ui/player/MiniPlayer.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/player/MiniPlayer.kt
@@ -37,6 +37,7 @@ import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Replay
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
